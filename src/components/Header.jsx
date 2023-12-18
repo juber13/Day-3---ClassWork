cebd19982b16109d9c31e1ae7e73430ce79fbf5a
juber <juberk1010@gmail.com>
2023-12-18 23:42:36 +0530

    make all ui

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 
 const Header = () => {
     return (
-        <header className="w-full bg-black text-white h-15 p-5 items-center">
+        <header className="w-full bg-black text-white h-15 p-5 items-center fixed z-10">
           <nav className="flex justify-between w-4/5  m-auto items-center">
             <div className="logo">
                 <ul className="flex gap-2 items-center">
@@ -11,7 +11,7 @@ const Header = () => {
             </div>
             <div className="center">
                 <ul className="flex gap-6 font-medium">
-                    <li className="text-500-blue"><a href="#">Home</a></li>
+                    <li className="text-blue-500"><a href="#">Home</a></li>
                     <li><a href="#">Quote</a></li>
                     <li><a href="#">Restaurents</a></li>
                     <li><a href="#">Foods</a></li>
